e02200d72046eade7699ef62cc347d813eb3b186
Paul Gazzillo <gazzillo@cs.nyu.edu>
2012-08-11 01:43:08 -0400

    added more features, better output formatting

--- t.sh
+++ t.sh
@@ -9,11 +9,10 @@ if [[ $# -eq 0 || $1 == "help" ]]; then
         "USAGE:\n" \
         "start [backdate] -- start timing\n" \
         "stop [backdate] -- stop timing (prompts for message)\n" \
-        "peek -- show time this period\n" \
-        "status -- show time this period\n" \
+        "peek|status -- show time this period\n" \
         "message message -- describe period\n" \
         "cancel -- cancel period\n" \
-        "day [ago] -- show time today or previous days\n" \
+        "today|day [ago] -- show time today or previous days\n" \
         "week [ago] -- show time this or past weeks\n" \
         "all -- show all time recorded\n" \
         "entry \"startdate\" \"enddate\" message\n" \
@@ -26,6 +25,10 @@ shift
 
 if [ $command == "status" ]; then
     command="peek"
+fi
+
+if [ $command == "today" ]; then
+    command="day"
 fi
 
 if [ ! -e $file ]; then
@@ -48,6 +51,43 @@ if [ -e $curr ]; then
         exit
     fi
 fi
+
+
+function dur2str {
+    local duration=$1
+    local hours=$(expr $duration / 3600)
+    local minutes=$(expr $duration % 3600 / 60)
+    local seconds=$(expr $duration % 60)
+    local timestr=
+    local timelabel=
+    if [ $hours -gt 0 ]; then
+        if [[ $hours -eq 1 && $minutes -eq 0 && $seconds -eq 0 ]]; then
+            timelabel="hour"
+        else
+            timelabel="hours"
+        fi
+        timestr="$hours:"
+    fi
+    if [ $minutes -gt 0 ]; then
+        if [ "$timelabel" == "" ]; then
+            if [[ $minutes -eq 1 && $seconds -eq 0 ]]; then
+                timelabel="minute"
+            else
+                timelabel="minutes"
+            fi
+        fi
+        timestr="$timestr$minutes:"
+    fi
+    if [ "$timelabel" == "" ]; then
+        if [ $seconds -eq 1 ]; then
+            timelabel="second"
+        else
+            timelabel="seconds"
+        fi
+    fi
+    timestr="$timestr$seconds"
+    echo "$timestr $timelabel"
+}
 
 if [ $command == "start" ]; then
     if [[ ! -e $curr ]]; then
@@ -82,40 +122,9 @@ elif [[ $command == "stop" || $command == "peek" ]]; then
         now=`echo "date $backdate +%Y/%m/%d\ %H:%M:%S" | bash`
         thissec=`echo "date $backdate +%s" | bash`
         duration=$(expr $thissec - $lastsec)
-        hours=$(expr $duration / 3600)
-        minutes=$(expr $duration % 3600 / 60)
-        seconds=$(expr $duration % 60)
-        timestr=
-        timelabel=
-        if [ $hours -gt 0 ]; then
-            if [[ $hours -eq 1 && $minutes -eq 0 && $seconds -eq 0 ]]; then
-                timelabel="hour"
-            else
-                timelabel="hours"
-            fi
-            timestr="$hours:"
-        fi
-        if [ $minutes -gt 0 ]; then
-            if [ "$timelabel" == "" ]; then
-                if [[ $minutes -eq 1 && $seconds -eq 0 ]]; then
-                    timelabel="minute"
-                else
-                    timelabel="minutes"
-                fi
-            fi
-            timestr="$timestr$minutes:"
-        fi
-        if [ "$timelabel" == "" ]; then
-            if [ $seconds -eq 1 ]; then
-                timelabel="second"
-            else
-                timelabel="seconds"
-            fi
-        fi
-        timestr="$timestr$seconds"
-        echo "$timestr $timelabel"
+        echo "duration $(dur2str $duration)"
+        lines=`cat $curr | wc -l`
         if [ $command == "stop" ]; then
-            lines=`cat $curr | wc -l`
             if [ "$lines" == "2" ]; then
                 message=
                 while [ "$message" == "" ]; do
@@ -129,6 +138,25 @@ elif [[ $command == "stop" || $command == "peek" ]]; then
             tail -n1 $file
         else
             echo "started $last"
+            if [ $lines -gt 2 ]; then
+                echo "message: `tail -n1 $curr`"
+            fi
+        fi
+    else
+        echo "the timer is not going"
+    fi
+elif [ $command == "message" ]; then
+    if [ -e $curr ]; then
+        lines=`cat $curr | wc -l`
+        if [ "$lines" == "2" ]; then
+            message=
+            while [ "$message" == "" ]; do
+                read -p "please enter a mesage: " message
+            done
+            echo "$message" >> $curr
+        else
+            echo "the message is already set"
+            echo "message: `tail -n1 $curr`"
         fi
     else
         echo "the timer is not going"
@@ -140,12 +168,54 @@ elif [ $command == "cancel" ]; then
         y|Y) rm $curr; echo "cancelled timer";;
         *) echo "aborted.  did not cancel the timer";;
     esac
-elif [ $command == "day" ]; then
-    echo "not yet implemented"
-    exit
-elif [ $command == "week" ]; then
-    echo "not yet implemented"
-    exit
+elif [[ $command == "day" || $command == "week" ]]; then
+    if [ $command == "day" ]; then
+        since=`date -d 00:00 +%s`
+    elif [ $command == "week" ]; then
+        since=`date -d "last $weekstart" +%s`
+    fi
+    until=`date +%s`
+    tempfile=`tempfile -d /tmp/ -p time`
+    cat $file | tail -n +2 | while read line; do
+        sdate=`echo $line | awk '{print $1, $2}'`
+        s=`date -d "$sdate" +%s`
+        edate=`echo $line | awk '{print $3, $4}'`
+        e=`date -d "$edate" +%s`
+        if [[ $s -gt $since && $s -lt $until || $e -gt $since && $e -lt $until ]]; then
+            if [[ $s -lt $since ]]; then
+                s=$since
+            fi
+            if [[ $e -gt $until ]]; then
+                e=$until
+            fi
+            thisduration=$((e - s))
+            echo $thisduration >> $tempfile
+            echo $(dur2str $thisduration) `echo $line | cut -d" " -f5-`
+        fi
+    done
+    if [ -e $curr ]; then
+        sdate=`head -n2 $curr | tail -n1`
+        s=`date -d "$sdate" +%s`
+        e=`date +%s`
+        if [[ $s -gt $since && $s -lt $until || $e -gt $since && $e -lt $until ]]; then
+            if [[ $s -lt $since ]]; then
+                s=$since
+            fi
+            if [[ $e -gt $until ]]; then
+                e=$until
+            fi
+            thisduration=$((e - s))
+            echo $thisduration >> $tempfile
+            echo "$(dur2str $thisduration) (current timer)"
+        fi
+    fi
+    duration=0
+    for i in `cat $tempfile`; do
+        duration=$((duration + i))
+    done
+    rm $tempfile
+    echo
+    echo $(dur2str $duration)
 elif [ $command == "all" ]; then
     echo "not yet implemented"
     exit
