08de9495531dea835a27360d0e7ddf3456d26a79
Paul Gazzillo <gazzillo@cs.nyu.edu>
2012-11-23 21:17:35 -0500

    return error state and msg

--- TimesheetLog.py
+++ TimesheetLog.py
@@ -8,17 +8,20 @@ class TimesheetLog(object):
   def __init__(self, filename):
     self.filename = filename
 
+  # Returns bool added, str errmessage
   def AddEntry(self, start_date, stop_date, message):
-    assert isinstance(start_date, datetime)
-    assert isinstance(stop_date, datetime)
-    assert '\n' not in message
+    if not isinstance(start_date, datetime):
+      return False, 'start date must be a datetime object'
+    if not isinstance(stop_date, datetime):
+      return False, 'stop date must be a datetime object'
+    if '\n' in message:
+      return False, 'message cannot have newline characters'
 
     if os.path.exists(self.filename):
       with open(self.filename, 'r') as fh:
         protocol_to_check = fh.readline().strip()
         if protocol_to_check != 'timesheet':
-          print "invalid timesheet file"
-          exit(1)
+          return False, 'invalid timesheet file'
     else:
       with open(self.filename, 'w') as fh:
         fh.write("timesheet\n")
@@ -27,4 +30,5 @@ class TimesheetLog(object):
       fh.write('%s %s %s\n' % (util.date2string(start_date),
                                util.date2string(stop_date),
                                message))
+    return True, ''
 
