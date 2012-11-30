c2b5c857dd0a3b4b63c8a2c262afb4e3fc8b6e63
Paul Gazzillo <gazzillo@cs.nyu.edu>
2012-11-30 00:15:08 -0500

    more options for displaying time deltas

--- util.py
+++ util.py
@@ -20,7 +20,7 @@ def date2string(date):
   assert isinstance(date, datetime)
   return date.strftime('%Y/%m/%d %H:%M:%S')
 
-def delta2string(delta):
+def delta2string(delta, show_days=False, decimal=False, abbr=False):
   assert isinstance(delta, timedelta)
   days = delta.days
   seconds = delta.seconds
@@ -28,22 +28,54 @@ def delta2string(delta):
   seconds = seconds % 60
   hours = minutes / 60
   minutes = minutes % 60
+  if not show_days:
+    hours += 24 * days
+    days = 0
   pl = ''
   if days > 0:
-    if days > 1 or hours > 0 or minutes > 0 or seconds > 0:
-      pl = 's'
-    return "%d:%02d:%02d:%02d" % (days, hours, minutes, seconds) + " day" + pl
+    if not decimal:
+      number_str = "%d:%02d:%02d:%02d" % (days, hours, minutes, seconds)
+    else:
+      number_str = '%.1f' % (days + float(hours) / 24)
+    if abbr:
+      units = ' d'
+    else:
+      if days > 1 or hours > 0 or minutes > 0 or seconds > 0:
+        pl = 's'
+      units = ' day' + pl
+    return number_str + units
   elif hours > 0:
-    if hours > 1 or minutes > 0 or seconds > 0:
-      pl = 's'
-    return "%d:%02d:%02d" % (hours, minutes, seconds) + " hour" + pl
+    if not decimal:
+      number_str = "%d:%02d:%02d" % (hours, minutes, seconds)
+    else:
+      number_str = '%.1f' % (hours + float(minutes) / 60)
+    if abbr:
+      units = ' hr'
+    else:
+      if hours > 1 or minutes > 0 or seconds > 0:
+        pl = 's'
+      units = ' hour' + pl
+    return number_str + units
   elif minutes > 0:
-    if minutes > 1 or seconds > 0:
-      pl = 's'
-    return "%d:%02d" % (minutes, seconds) + " minute" + pl
+    if not decimal:
+      number_str = "%d:%02d" % (minutes, seconds)
+    else:
+      number_str = "%.1f" % (minutes + float(seconds) / 60)
+    if abbr:
+      units = ' min'
+    else:
+      if minutes > 1 or seconds > 0:
+        pl = 's'
+      units = ' minute' + pl
+    return number_str + units
   elif seconds > 0:
-    if seconds > 1:
-      pl = 's'
-    return "%d" % (seconds) + " second" + pl
+    number_str = "%d" % (seconds)
+    if abbr:
+      units = ' sec'
+    else:
+      if seconds > 1:
+        pl = 's'
+      units = ' second' + pl
+    return number_str + units
   else:
     return None
