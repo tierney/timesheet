c77d38af6fac4d1ad0fd8e8cdc0c6974a32044c4
Paul Gazzillo <gazzillo@cs.nyu.edu>
2012-11-30 00:14:13 -0500

    Use python property to get entries.  Now ensures that the entries are sorted when reading them.

--- TimesheetCSV.py
+++ TimesheetCSV.py
@@ -4,12 +4,50 @@ from datetime import datetime
 import util
 import os
 import csv
+import operator
 
 class TimesheetCSV(object):
   def __init__(self, filename):
     self.filename = filename
+    self.list = None
+    self._entries = None
 
-  # Returns bool added, str errmessage
+  # always has the entries in the database as a list.  None means invalid timesheet file
+  @property
+  def entries(self):
+    if self._entries == None:
+      if os.path.exists(self.filename):
+        needs_sorting = False
+        with open(self.filename, 'rb') as fh:
+          reader = csv.reader(fh, delimiter=',')
+          self._entries = list(reader)
+
+          # check need to sort
+          last = None
+          for row in self._entries:
+            start_date = row[0]
+            if last == None:
+              last = start_date
+            elif start_date < last:
+              print row
+              needs_sorting = True
+              break
+            else:
+              last = start_date
+
+        if needs_sorting:
+          # TODO verbose output
+          self._entries = sorted(self._entries, key=operator.itemgetter(0))
+          with open(self.filename, 'wb+') as fh:
+            writer = csv.writer(fh, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for row in self._entries:
+              writer.writerow(row)
+      else:
+        self._entries = []
+
+    return self._entries
+
+  # add a new entry
   def AddEntry(self, start_date, stop_date, message):
     if not isinstance(start_date, datetime):
       return False, 'start date must be a datetime object'
@@ -18,20 +56,25 @@ class TimesheetCSV(object):
     if '\n' in message:
       return False, 'message cannot have newline characters'
 
-    if os.path.exists(self.filename):
-      reader = csv.reader(self.filename, delimiter=',')
-      # check for duplicate entries
-      # hashkey = util.date2string(start_date)+util.date2string(stop_date)
-      # search backwards in log for same hash key
-      # open file
-      # iterate over all entries (need 
-    
-    with open(self.filename, 'a+') as csvfile:
+    # TODO cache recent entry hashes
+
+    # check for duplicate entries
+    new_hash = hash(util.date2string(start_date)+util.date2string(stop_date))
+    for row in reversed(self.entries):
+      item_start = row[0]
+      item_stop = row[1]
+      item_hash = hash(item_start+item_stop)
+      if new_hash == item_hash:
+        return False, "duplicate entry"
+
+    with open(self.filename, 'ab+') as csvfile:
       writer = csv.writer(csvfile, delimiter=',',
-                          quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                          quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
       writer.writerow([util.date2string(start_date),
                        util.date2string(stop_date),
                        message])
 
-    return True, ''
+    # force reread of timesheet
+    self._entries = None
 
+    return True, ''
