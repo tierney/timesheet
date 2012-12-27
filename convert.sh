e7a48862e50e2c5d94f00fdf79de4db153771b7f
Paul Gazzillo <gazzillo@cs.nyu.edu>
2012-12-27 06:58:58 -0500

    removed extraneous files

--- convert.sh
+++
@@ -1,9 +0,0 @@
-read i
-if [ "$i" != "timesheet" ]; then
-    echo "only can convert timesheet files"
-    exit 1
-fi
-while read i; do
-    echo "`echo $i | cut -c1-19`,`echo $i | cut -c21-39`,\"`echo $i | cut -c41-`\""
-done
-
